8262e0ffa91420897021b7e9786f9262a0ea4493
Flaudz <nico936d@edu.campusvejle.dk>
2021-01-11 12:12:24 +0100

    changed so footer.php doesint contain the script tag to the silder

---
+++ Opgave/register.php
@@ -0,0 +1,9 @@
+<?php
+$title = "Register | FancyClothes.dk";
+$description = "FancyClothes register side er en fatastisk side hvor du kan register dig som bruger på vores fatastiske side";
+include "includes/header.php";
+?>
+
+<?php
+include "includes/footer.php";
+?>
