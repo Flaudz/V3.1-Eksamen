dd3eafb3274092f3079e38a573c3d1e6509bccce
Flaudz <nico936d@edu.campusvejle.dk>
2021-01-12 12:17:12 +0100

    Made the api with 2 end points

--- Opgave/api/app.js
+++ Opgave/api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) =>{
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+const productRoutes = require('./routes/products');
+
+app.use('/products', productRoutes);
 
 module.exports = app;
+
+// <?php
+// $response = file_get_contents('http://localhost:3000/');
+// echo $response;
+// ?>
