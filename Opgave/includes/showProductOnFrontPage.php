77679399ad8a68f99ae0989df9c72845f0a6c33f
Flaudz <nico936d@edu.campusvejle.dk>
2021-01-12 09:20:15 +0100

    I deleted showProductOnFrontPage.php and made it again

---
+++ Opgave/includes/showProductOnFrontPage.php
@@ -0,0 +1,47 @@
+<?php
+include 'database.php';
+
+// Making a SQL stmt to get information from the database(We get $con from the database.php)
+$stmt = $con->prepare("SELECT * FROM products");
+$stmt->execute();
+// Here i check if the row count (Number of rows i got out of the SQl) is above 0
+if ($stmt->rowCount() > 0) {
+    // If it is then do a while loop with fetch to get the results
+    while ($row = $stmt->fetch()) {
+        $number = intval($row['stars']);
+
+?>
+        <article>
+            <img src="<?= $row['imgSrc'] ?>" alt="<?= $row['imgAlt'] ?>">
+            <div class="info">
+                <h3><?= $row['headline'] ?></h3>
+                <div class="stars">
+                    <?php
+                    for ($i = 0; $i < intval($row['stars']); $i++) {
+                    ?>
+                        <i class='fa fa-star' aria-hidden='true'></i>
+                    <?php
+                    }
+                    while ($number < 5) {
+                    ?>
+                        <i class='fa fa-star-o' aria-hidden='true'></i>
+                    <?php
+                        $number++;
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="description">
+                <div class="published">
+                    Oprettet: Mandag d. 24/6-2019 af Mark
+                </div>
+                <p><?= $row['bodyText'] ?>
+                    <p class="readmore">Læs mere...</p>
+                </p>
+                <!-- Mulighed for sletning herunder -->
+            </div>
+        </article>
+<?php
+    }
+}
+?>
