19cd575516c80a42aa7e6cf3bc7e18ad1df0f77d
Flaudz <nico936d@edu.campusvejle.dk>
2021-01-13 09:58:13 +0100

    Did so you can upload images insted of remembering the name of the picture

--- Opgave/includes/insertArticle.php
+++ Opgave/includes/insertArticle.php
@@ -1,11 +1,23 @@
 <?php
 include "database.php";
+
+// Upload Image
+// Tjekker hvis jeg har sat et billede ind
+if(isset($_FILES['image'])) {
+    // name stores the original filename from the client before it was uploaded
+    $file_name = $_FILES['image']['name'];
+    // tmp_name stores the name of the temporary file
+    $file_tmp = $_FILES['image']['tmp_name'];
+    move_uploaded_file($file_tmp, "../img/" . $_FILES["image"]["name"]);
+}
+
+
 // Params
 $headline = $_POST['heading'];
 $content = $_POST['content'];
 $stars = intval($_POST['stars']);
 $category = intval($_POST['category']);
-$imgSrc = $_POST['imgSrc'];
+$imgSrc = "img/" . $_FILES["image"]["name"];
 $imgAlt = $_POST['imgAlt'];
 $gender = $_POST['gender'];
 
